2db7e2c15a1cddf04b96dd9c099f62d70e0cd11d
Jorge Monasterio <metalgeorge894@gmail.com>
2023-03-13 18:51:56 -0300

    Se llego al ejercicio 8 de los extras

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -153,7 +153,7 @@ public class JavaApplication5 {
         System.out.println("la cantidad de secuencias incorrectas es de " + incorrecto);
          */
         //EJERCICIO 8
-           /* 
+        /* 
                 int i;
 		int j;
 		int num;
@@ -169,11 +169,9 @@ public class JavaApplication5 {
 			}
 			System.out.println("");
 		}
-        */
-           
-           
-           //EJERCICIO 1 EXTRA
-           
+         */
+        //EJERCICIO 1 EXTRA
+        /*
            int minutos,horas,dias;
            
            System.out.println("ingresa la cantidad de minutos");
@@ -191,8 +189,187 @@ public class JavaApplication5 {
            
            System.out.println("la cantidad de " + minutos + " minutos equivale a " + dias + " dias y " + horas + " horas");
            
+         */
+ /*EJERCICIO 2 EXTRA
+           
+           /*
+           int a,b,c,d,aux;
+           System.out.println("ingresa 4 valores");
+           
+           a= leer.nextInt();
            
            
+           b= leer.nextInt();
+
+           c= leer.nextInt();
+           
+           d= leer.nextInt();
+           
+           aux=b;
+           b=c;
+           c=a;
+           a=d;
+           d=aux;
+           
+           System.out.println("A es igual: " +a);
+           System.out.println("B es igual: " +b);
+           System.out.println("C es igual: " +c);
+           System.out.println("D es igual: " +d);
+           
+         */
+ /* EJERCICIO 3 EXTRA
+ 
+            String vocal;
+            
+            System.out.println("ingresa una letra");
+            vocal=leer.next();
+            
+            if (vocal.equalsIgnoreCase("a") || vocal.equalsIgnoreCase("e") || vocal.equalsIgnoreCase("i") || vocal.equalsIgnoreCase("o") || vocal.equalsIgnoreCase("u")) {
+                
+                System.out.println("has ingresado una vocal");
+            }else   
+                System.out.println("No has ingresado una vocal");
+            
+        }
+ 
+         */
+        //EJERCICIO 4 EXTRA
+        /*
+ int num;
+ 
+        System.out.println("ingresa un numero");
+        num = leer.nextInt();
+        
+        switch (num){
+            
+            case 1: 
+                System.out.println("en romano es I" );
+                break;
+            case 2:
+                System.out.println("en romano es II");
+                break;
+            case 3:
+                System.out.println("en romano es III");
+                break;
+            case 4:
+                System.out.println("en romano es IV");
+                break;
+            case 5:
+                System.out.println("en romano es V");
+                break;
+            case 6:
+                System.out.println("en romano es VI");
+                break;
+            case 7:
+                System.out.println("en romano es VII");
+                break;
+            case 8:
+                System.out.println("en romano es VIII");
+                break;
+            case 9:
+                System.out.println("en romano es IX");
+                break;
+            case 10:
+                System.out.println("en romano es X");
+                break;
+            default:
+                System.out.println("has ingresado un numero incorrecto");
+        }
+
+
+         */
+        //EJERCICIO 6 EXTRA
+        /*
+        char letra;
+        double costo;
+
+        System.out.println("ingresa el tipo de socio entra A y C");
+        letra = leer.next().charAt(0);
+
+        switch (letra) {
+            case 'a':
+                System.out.println("Socio A");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                costo = costo - (costo * 0.5);
+                System.out.println("Con el 50% de descuento el costo le queda en: " + costo);
+                break;
+            case 'A':
+                System.out.println("Socio A");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                costo = costo - (costo * 0.5);
+                System.out.println("Con el 50% de descuento el costo le queda en: " + costo);
+                break;
+
+        
+            case 'b':
+                System.out.println("Socio B");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                costo = costo - (costo * 0.35);
+                System.out.println("Con el 35% de descuento el costo le queda en: " + costo);
+                break;
+            case 'B':
+                System.out.println("Socio B");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                costo = costo - (costo * 0.35);
+                System.out.println("Con el 35% de descuento el costo le queda en: " + costo);
+                break;
+                
+            case 'c':
+                System.out.println("Socio C");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                
+                System.out.println("Como es un usuario pelabola,  el costo le queda en: " + costo);
+                break;
+            case 'C':
+                System.out.println("Socio C");
+                System.out.println("Ingresa el costo del tratamiento");
+                costo = leer.nextDouble();
+                
+                System.out.println("Como es un usuario pelabola,  el costo le queda en: " + costo);
+                break;
+
+        }
+
+*/
+        /*
+        //EJERCICIO 7 EXTRA
+        double estatura, promedio_b,promedio_g,estatura_baja,estatura_g;
+        estatura_baja=0;
+        estatura_g=0;
+        int personas,i;
+        int personasbajas=0;
+        i=0;
+        
+        System.out.println("ingresa la cantidad de personas a medir estatura");
+        personas=leer.nextInt();
+        do {
+            System.out.println("ingresa de la persona " +(i+1));
+            
+            estatura=leer.nextDouble();
+           
+            if (estatura<=1.6) {
+                estatura_baja+=estatura;
+                personasbajas++;
+            }
+              
+            estatura_g+=estatura;
+            
+            i++;
+        } while (i<personas);
+        
+ promedio_b=estatura_baja/personasbajas;
+ promedio_g=estatura_g/personas;
+ 
+        System.out.println("el promedio de estatura de los enanos es de: " +promedio_b);
+        System.out.println("el promedio de estatura de las personas ingresadas: " +promedio_g);
+        
+        
+        */
 
     }
 
