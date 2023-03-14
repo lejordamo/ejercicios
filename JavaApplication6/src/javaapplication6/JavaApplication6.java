267ac99701091da0a76540d2fd907539ac09f0f0
Jorge Monasterio <metalgeorge894@gmail.com>
2023-03-14 18:36:48 -0300

    se llego al ejercicio 11

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -143,6 +143,82 @@ public class JavaApplication6 {
         System.out.println("la cantidad de numeros pares es: " + pares);
         System.out.println("la cantidad de numeros impares es: " + impares);
 */
+        //EJERCICIO 9
+        /*
+        int divisor,dividendo,cosiente,residuo;
+        cosiente=0;
+        
+        System.out.println("ingresa el numero dividendo");
+        dividendo=leer.nextInt();
+        System.out.println("ingresa el numero divisor");
+        divisor=leer.nextInt();
+        
+        //50-13=37
+        
+        residuo=dividendo;
+        do {
+            residuo=residuo-divisor;
+            cosiente++;
+                        
+        } while (residuo>=divisor);
+        
+        
+        System.out.println("el residuo de la division es: " +residuo);
+        System.out.println("el cosiente de la division es: " +cosiente);
+
+        */
+        
+        
+        
+        //EJERCICIO 10
+        /*
+        int resultado,multi;
+        int num1=(int)((Math.random()+0.1) * 10);
+        int num2=(int)((Math.random()+0.1) * 10);
+                
+       multi = num1*num2;
+        //System.out.println(num1);
+        //System.out.println(num2);
+        //System.out.println(multi);
+
+        
+        
+        
+        System.out.println("ingresa el resultado de la multiplicacion de los numeros ocultos, no mayor que 100");
+        resultado=leer.nextInt();
+        
+        do {
+            
+            System.out.println("intenta nuevamente");
+            resultado=leer.nextInt();
+            
+            if (resultado==multi) {
+                System.out.println("felicidades has encontrado el resultado de la multiplicacion de los numeros " +num1 + " * " + num2);
+                
+                break;
+                
+            }
+        } while (resultado!=multi);
+        
+*/
+        
+        //EJERCICIO 10
+        /*
+        long numero,contador;
+        contador=0;
+        System.out.println("ingresa un numero");
+        numero=leer.nextLong();
+        
+        do {
+            
+            numero=numero/10;
+            contador++;
+            
+            
+        } while (numero!=0);
+        
+        System.out.println("la cantidad de digitos del numero ingresado es: " +contador);
+        */
         
         
         
