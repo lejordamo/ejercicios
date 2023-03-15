7e2773be30d4f23983e30bd7104a4584ca649cca
Jorge Monasterio <metalgeorge894@gmail.com>
2023-03-15 17:43:11 -0300

    ejercicios 11 y 12 de la guia lectura

---
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author George
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+
+        //EJERCICIO 11 GUIA LECTURA
+        /*
+        
+        
+        String frase;
+
+        System.out.println("ingresa una frase");
+        frase = leer.nextLine();
+
+        System.out.println("la frase es " + cambiarfrase(frase));
+
+    }
+
+    public static String cambiarfrase(String frase) {
+        String nuevafrase = "";
+        String letra;
+        for (int i = 0; i < frase.length(); i++) {
+            letra = frase.substring(i, i + 1);
+            switch (frase.substring(i, i + 1)) {
+
+                case "a":
+                    letra = "@";
+
+                    break;
+                case "e":
+                    letra = "#";
+
+                    break;
+                case "i":
+                    letra = "$";
+
+                    break;
+                case "o":
+                    letra = "%";
+
+                    break;
+                case "u":
+                    letra = "*";
+
+                    break;
+
+            }
+            nuevafrase = nuevafrase.concat(letra);
+        }
+        frase = nuevafrase;
+        return frase;
+
+         */
+        //EJERCICIO 12 GUIA LECTURA
+        /*
+        int num1, num2;
+
+        System.out.println("ingresa 2 numeros");
+        num1 = leer.nextInt();
+        num2 = leer.nextInt();
+
+        esMultiplo(num1, num2);
+
+    }
+
+    public static void esMultiplo(int num1, int num2) {
+
+        if (num1 % num2 == 0) {
+
+            System.out.println("el numero 1 es multiplo del numero 2");
+
+        } else {
+            System.out.println("no es multiplo");
+        }
+
+    }*/
+       
+             
+        
+       
+    }
+
+}
