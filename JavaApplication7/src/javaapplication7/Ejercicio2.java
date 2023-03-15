4cf8ae3c0a415420848a03187e94cf3c33b56cb3
Jorge Monasterio <metalgeorge894@gmail.com>
2023-03-15 18:56:16 -0300

    termino el ejercio 2

--- JavaApplication7/src/javaapplication7/Ejercicio2.java
+++ JavaApplication7/src/javaapplication7/Ejercicio2.java
@@ -5,17 +5,66 @@
  */
 package javaapplication7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author George
  */
 public class Ejercicio2 {
+    Scanner leer= new Scanner(System.in);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        mostrar();
+        
+    }
+    
+    public static void mostrar(){
+        Scanner leer= new Scanner(System.in);
+        
+        System.out.println("ingresa la cantidad de personas");
+        int n=leer.nextInt();
+        int edad;
+        String nombre,respuesta="Si";
+        
+        do {
+            System.out.println("ingresa un nombre");
+            nombre=leer.next();
+            System.out.println("ingresa la edad");
+            edad=leer.nextInt();
+            
+            if (edad>=18) {
+                
+                System.out.println(nombre + " es mayor de edad");
+            }else{
+                 System.out.println(nombre + " es menor de edad");
+        
+        }
+            do {
+                System.out.println("desea continuar?");
+            respuesta=leer.next();
+                
+            } while (!respuesta.equalsIgnoreCase("Si") && !respuesta.equalsIgnoreCase("No"));
+            
+  
+            
+            
+            
+            n--;
+        } while (n>0 && !respuesta.equalsIgnoreCase("No") );
+        
+       
+        
+        
+        
+        
     }
     
 }
